fa010950598457ca9b79766a02bd00978cb568a8
Davichobits <davidruizdeveloper@gmail.com>
2025-08-26 03:39:55 -0500

    Global Styles variables added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { CounterPageComponent, PasswordGeneratorPageComponent } from './pages';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: PasswordGeneratorPageComponent,
+  },
+  {
+    path: 'counter',
+    component: CounterPageComponent,
+  }
+];
---
+++ src/app/pages/password-generator/password-generator-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './password-generator-page.html'
+})
+
+export class PasswordGeneratorPageComponent {
+
+}
